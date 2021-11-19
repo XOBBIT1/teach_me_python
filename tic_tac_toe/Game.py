da4bbcaa06851245e51f431deaec58f97afc1ec9
37533 <ben300300@gmail.com>
2021-11-19 12:35:48 +0300

    NEW TASK

--- tic_tac_toe/Game.py
+++ tic_tac_toe/Game.py
@@ -1,36 +1,8 @@
 from itertools import cycle
 from Steps import step_from_user
-from user import METHODS, PATTEREN_USER
+from user import METHODS
 from board import match_matrix, creat_board, board_print
 from Constant import SYMBOLS
-
-inter_str = {
-    "Rules": "",
-    "Hello": "Добро пожаловать в Крестики/нолики ",
-    "Name": "Введите имя",
-    "Mod": "С кем вы будете играть? {variants}",
-    "Ask_step": "Ход #{step_number} грока {name}",
-    "Win": "Победу одержал {name} на ходу {step_number}",
-    "New_game":"Желаете насать новую игру ? {variants}",
-    "Draw": "Ничья"
-}
-
-pattern_variants = {
-    "Type": lambda pattern, **kwargs: pattern.format(variants = ("C","U") ),#будем хранить функции, которые будут возвращать готовую строку, учитывая данные, которые получит
-    "Ask_step": lambda pattern, **kwargs: pattern.format(**kwargs),#будем хранить функции, которые будут возвращать готовую строку
-    "Win":lambda pattern, **kwargs: pattern.format(**kwargs),#будем хранить функции, которые будут возвращать готовую строку
-    "New_game": lambda pattern, **kwargs: pattern.format(variants = ("Y","N")),#будем хранить функции, которые будут возвращать готовую строку, учитывая данные, которые получит
-    "Enter_name": lambda pattern, **kwargs: pattern.format(**kwargs)#будем хранить функции, которые будут возвращать готовую строку
-}
-
-
-def interface_user(pattern_name, **pattern_var):# на вход принемает один из ключей словаря pattern_variants, **pattern_var является **kwargs для универсальности
-    if pattern_name in pattern_variants:# если pattern_name находится в pattern_variants
-        ask_str = pattern_variants[pattern_name](inter_str[pattern_name], **pattern_var)#  так как ключом является lambda, то и на вход мы запрашиваем значения функции
-        user_input = input(ask_str)# спрашиваем пользователя
-        return user_input# возращаем ответ
-    else:
-        print(inter_str[pattern_name])  # в ином случае просто выводим значение введённго ключа
 
 
 def game(users: list[dict, ...], board: list[list], step_num): # на вход запрашивает пользователя, который является списком внутри которого словарь, а также доску(список списков )
